449c66cf0ae3c77fed45fe7a04a7f6f0ea168ccb
chandradityadebnath <chandradityadebnath@gmail.com>
2026-07-15 13:19:43 +0530

    Refactor RetailApp for improved structure and readability

--- dashboard.py
+++ dashboard.py
@@ -23,7 +23,7 @@ class RetailApp:
             self.db = mysql.connector.connect(
                 host=os.getenv("DB_HOST"),
                 user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD",
+                password=os.getenv("DB_PASSWORD"),
                 database=os.getenv("DB_NAME")
             )
             self.cursor = self.db.cursor()
